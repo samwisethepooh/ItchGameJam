063c79522fb401fc4fa816216e9333c240cfc93e
Sam Holder <samholder@live.co.uk>
2022-11-08 20:14:20 +0000

    added gate and gate sounds.

---
+++ Source/Level/Gateway.cs
@@ -0,0 +1,38 @@
+using Godot;
+
+public partial class Gateway : Node3D, IActivatable
+{
+    private AnimationPlayer _animationPlayer;
+    private CharacterBody3D _lastInvoker;
+    private bool _open = false;
+
+    [Export] public bool Open { get => _open; set => UpdateOpen(value); }
+
+    public override void _Ready()
+    {
+        _animationPlayer = GetNode<AnimationPlayer>("./AnimationPlayer");
+    }
+
+    public void UpdateOpen(bool open)
+    {
+        _open = open;
+        PlayAnimation();
+    }
+
+    private void PlayAnimation()
+    {
+        if (Open)
+        {
+            _animationPlayer.Play("Open");
+        }
+        else
+        {
+            _animationPlayer.Play("Close");
+        }
+    }
+
+    public void Activate(bool isActive, Node3D source, CharacterBody3D invoker)
+    {
+        Open = isActive;
+    }
+}
